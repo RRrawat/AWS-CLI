b3d9a68275e0fd5879d44f804d0a037849d2e8a5
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-01 10:08:19 +0530

    Update ec2.sh

--- EC2-vpc/ec2.sh
+++ EC2-vpc/ec2.sh
@@ -1,4 +1,13 @@
+#it will launch a t2.micro instance in the specified subnet of a VPC. Replace the italicized parameter values with your own.
 aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
+
+#To add a block device to your instance, specify the --block-device-mappings option when you use run-instances.
+#The following example parameter provisions a standard Amazon EBS volume that is 20 GB in size, and maps it to your instance using the identifier /dev/sdf.
+#--block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"VolumeSize\":20,\"DeleteOnTermination\":false}}]"
+
+#The following example adds an Amazon EBS volume, mapped to /dev/sdf, based on an existing snapshot. A snapshot represents an image that is loaded onto the volume for you.
+#--block-device-mappings "[{\"DeviceName\":\"/dev/sdf\",\"Ebs\":{\"SnapshotId\":\"snap-a1b2c3d4\"}}]"
+
 aws ec2 create-tags --resources i-5203422c --tags Key=Name,Value=MyInstance
 aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
 aws ec2 describe-instances --filters "Name=tag:Name,Values=MyInstance"
