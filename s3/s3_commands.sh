86100e0ce4cace1ddd49e15f0d57a8a44fcca072
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-08-22 09:30:30 +0530

    Update s3_commands.sh

--- s3/s3_commands.sh
+++ s3/s3_commands.sh
@@ -16,3 +16,9 @@ aws s3 mv <source> <target> [--options]
 
 #Copy objects
 aws s3 cp <source> <target> [--options]
+
+#The s3 cp command uses the following syntax to upload a file stream from stdin to a specified bucket.
+aws s3 cp - <target> [--options]
+
+#The s3 cp command uses the following syntax to download an Amazon S3 file stream for stdout.
+aws s3 cp <target> [--options] -
